5feee5de82d1265b73ef93f1c4d47624c117e64a
FidFenix <fid.mamani@gmail.com>
2020-04-17 08:27:00 -0500

    These are change for ejectign, because sass it is not working apparently

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App';
 ReactDOM.render(
-    <did></did>, document.getElementById("root")
+    <App />, document.getElementById("root")
 );
