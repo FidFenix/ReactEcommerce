e168e6312ef15c11cf5652d6b0289061f0ccaf13
FidFenix <fid.mamani@gmail.com>
2020-04-19 17:29:56 -0500

    NODEENV set for redux logger in heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import {persistStore} from 'redux-persist'; //b
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
-const middlewares = [logger];
+const middlewares = [];
 
+if(process.env.NODEEVN === 'development') {
+    middlewares.push(logger)
+}
 export const store = createStore(rootReducer, applyMiddleware( ...middlewares));
 
 export const persistor = persistStore(store); //b
