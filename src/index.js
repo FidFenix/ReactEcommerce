64bd3f6e3beb331a3683ee3fc2c65e6b47712998
FidFenix <fid.mamani@gmail.com>
2020-04-18 23:57:06 -0500

    Adding local storage to our app

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 //gives the entry point to the reducer
-import {Provider} from 'react-redux'; // as parent allows access to everything
-import store from './redux/store';
+import { Provider } from 'react-redux'; // as parent allows access to everything
+import { store, persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';  //include electron and react-native
+
 
 ReactDOM.render(
     <Provider store = { store }>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>
     , document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,27 @@
 //Global reducer
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //i want to use localstorage
+//import sessionStorage from ''
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-export default combineReducers({
-    user: userReducer,
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'] //contains any reducer passed
+}
+
+const rootReducer = combineReducers({
+    user: userReducer, 
     cart: cartReducer
 })
+
+/*export default combineReducers({
+    user: userReducer,  //we really no need to persist the user, since it handles firebase
+    cart: cartReducer
+})*/
+
+export default persistReducer(persistConfig, rootReducer);
